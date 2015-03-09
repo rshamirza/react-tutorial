6b49a990ac9242595ac462512dd0f1a4ad85d5c1
rshamirza <rshamirza@rgnt2-74-126-dhcp.int.colorado.edu>
2015-03-09 17:35:39 -0600

    completed milestone 1

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,13 +1,57 @@
-var CommentBox = React.createClass({
+// tutorial7.js
+var converter = new Showdown.converter();
+var Comment = React.createClass({
   render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
     return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
 });
+
+// tutorial2.js
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+
+// tutorial3.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+
 React.render(
-  <CommentBox />,
+  React.createElement(CommentBox, null),
   document.getElementById('content')
-)
+);
